0f903f20c0126570e80f7f1900805e9198b9c688
Bolaji06 <joshspencer0603@gmail.com>
2024-08-29 14:39:24 +0100

    make editor panel persistent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import "./App.css";
 import AceCodeEditor from "./components/ui/aceEditor";
-import logoIcon from "../public/play-svgrepo-com.svg";
+import logoIcon from "/play-svgrepo-com.svg";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+
+
+const defaultSize = [100, 100, 100]
+const panelSize = window.localStorage.getItem("run_resizable_panels")
+const size: number[] = panelSize ? JSON.parse(panelSize) : defaultSize;
 
 function App() {
   const [toggleScript, setToggleScript] = useState<boolean>(false);
 
   function handleToggleScript() {
     setToggleScript(!toggleScript);
+  }
+
+  function onLayout(sizes: number[]) {
+    window.localStorage.setItem("run_resizable_panels", JSON.stringify(sizes));
   }
 
   return (
@@ -32,75 +41,75 @@ function App() {
         <div className="w-full">
           {/* <PanelGroup direction="vertical">
             <Panel defaultSize={90}> */}
-              <PanelGroup direction="horizontal">
-                <div className="flex items-center w-full">
-                  <Panel className="w-full" defaultSize={100} minSize={10}>
-                    <div className="w-full">
-                      <button className="py-2 px-6 bg-gray-900 text-pink-700 text-sm">
-                        HTML
-                      </button>
-                      <AceCodeEditor
-                        width="100%"
-                        height="400px"
-                        name="html"
-                        mode="html"
-                      />
-                    </div>
-                  </Panel>
-
-                  <PanelResizeHandle className="w-2 h-full bg-slate-300" />
-
-                  <Panel defaultSize={100} className="w-full" minSize={10}>
-                    <div className="w-full">
-                      <button className="py-2 px-6 bg-gray-900 text-blue-500 text-sm">
-                        CSS
-                      </button>
-                      <AceCodeEditor
-                        width="100%"
-                        height="400px"
-                        name="css"
-                        mode="css"
-                      />
-                    </div>
-                  </Panel>
-
-                  <PanelResizeHandle className="w-2 h-full bg-slate-300" />
-
-                  <Panel defaultSize={100} minSize={10}>
-                    <div className="w-full">
-                      <button
-                        onClick={handleToggleScript}
-                        className={`py-2 px-6 bg-gray-900 ${
-                          !toggleScript ? "text-yellow-400" : "text-blue-500"
-                        } text-sm`}
-                      >
-                        {!toggleScript ? "JS" : "TS"}
-                      </button>
-                      {!toggleScript ? (
-                        <AceCodeEditor
-                          width="100%"
-                          height="400px"
-                          name="javascript"
-                          mode="javascript"
-                        />
-                      ) : (
-                        <AceCodeEditor
-                          width="100%"
-                          height="400px"
-                          name="typescript"
-                          mode="typescript"
-                        />
-                      )}
-                    </div>
-                  </Panel>
+          <PanelGroup direction="horizontal" onLayout={onLayout}>
+            <div className="flex items-center w-full">
+              <Panel className="w-full" defaultSize={size[0]} minSize={10} >
+                <div className="w-full">
+                  <button className="py-2 px-6 bg-gray-900 text-pink-700 text-sm">
+                    HTML
+                  </button>
+                  <AceCodeEditor
+                    width="100%"
+                    height="400px"
+                    name="html"
+                    mode="html"
+                  />
                 </div>
-              </PanelGroup>
-            {/* </Panel> */}
-            {/* <PanelResizeHandle className="w-full h-2 bg-slate-500" /> */}
+              </Panel>
 
-            {/* <Panel defaultSize={10}> */}
-              <div className="w-full h-20 overflow-y-auto bg-slate-200 border-[4px] border-slate-700"></div>
-            {/* </Panel>
+              <PanelResizeHandle className="w-2 h-full bg-slate-300" />
+
+              <Panel defaultSize={size[1]} className="w-full" minSize={10}>
+                <div className="w-full">
+                  <button className="py-2 px-6 bg-gray-900 text-blue-500 text-sm">
+                    CSS
+                  </button>
+                  <AceCodeEditor
+                    width="100%"
+                    height="400px"
+                    name="css"
+                    mode="css"
+                  />
+                </div>
+              </Panel>
+
+              <PanelResizeHandle className="w-2 h-full bg-slate-300" />
+
+              <Panel defaultSize={size[2]} minSize={10}>
+                <div className="w-full">
+                  <button
+                    onClick={handleToggleScript}
+                    className={`py-2 px-6 bg-gray-900 ${
+                      !toggleScript ? "text-yellow-400" : "text-blue-500"
+                    } text-sm`}
+                  >
+                    {!toggleScript ? "JS" : "TS"}
+                  </button>
+                  {!toggleScript ? (
+                    <AceCodeEditor
+                      width="100%"
+                      height="400px"
+                      name="javascript"
+                      mode="javascript"
+                    />
+                  ) : (
+                    <AceCodeEditor
+                      width="100%"
+                      height="400px"
+                      name="typescript"
+                      mode="typescript"
+                    />
+                  )}
+                </div>
+              </Panel>
+            </div>
+          </PanelGroup>
+          {/* </Panel> */}
+          {/* <PanelResizeHandle className="w-full h-2 bg-slate-500" /> */}
+
+          {/* <Panel defaultSize={10}> */}
+          <div className="w-full h-20 overflow-y-auto bg-slate-200 border-[4px] border-slate-700"></div>
+          {/* </Panel>
           </PanelGroup> */}
         </div>
       </section>
