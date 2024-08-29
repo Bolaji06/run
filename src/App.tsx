f7b515e114a1725338b52716f615fff322428c10
Bolaji06 <joshspencer0603@gmail.com>
2024-08-29 07:10:46 +0100

    make a composable editor component and make editors resizable

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,108 @@
+import { useState } from "react";
 import "./App.css";
-import CSSEditor from "./components/CssEditor";
-import HTMLEditor from "./components/HtmlEditor";
-import JavaScriptEditor from "./components/JavaScriptEditor";
+import AceCodeEditor from "./components/ui/aceEditor";
+import logoIcon from "../public/play-svgrepo-com.svg";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 function App() {
+  const [toggleScript, setToggleScript] = useState<boolean>(false);
+
+  function handleToggleScript() {
+    setToggleScript(!toggleScript);
+  }
+
   return (
     <>
       <section className="">
-        <div className="grid grid-cols-3">
-          <div className="">
-            <button className="py-2 px-6 bg-gray-900 text-pink-700 text-sm">
-              HTML
-            </button>
-            <HTMLEditor />
+        <header className="p-2 border-b-2 border-gray-900">
+          <div className="flex items-center gap-1">
+            <div>
+              <img
+                src={logoIcon}
+                alt="run logo"
+                className="w-4 aspect-square"
+              />
+            </div>
+            <div className="">
+              <h2 className="text-sm font-bold text-white pb-1">run</h2>
+            </div>
           </div>
+        </header>
 
-          <div className="">
-            <button className="py-2 px-6 bg-gray-900 text-blue-500 text-sm">CSS</button>
-            <CSSEditor />
-          </div>
+        <div className="w-full">
+          {/* <PanelGroup direction="vertical">
+            <Panel defaultSize={90}> */}
+              <PanelGroup direction="horizontal">
+                <div className="flex items-center w-full">
+                  <Panel className="w-full" defaultSize={100} minSize={10}>
+                    <div className="w-full">
+                      <button className="py-2 px-6 bg-gray-900 text-pink-700 text-sm">
+                        HTML
+                      </button>
+                      <AceCodeEditor
+                        width="100%"
+                        height="400px"
+                        name="html"
+                        mode="html"
+                      />
+                    </div>
+                  </Panel>
 
-          <div className="">
-            <button className="py-2 px-6 bg-gray-900 text-yellow-400 text-sm">
-              JS
-            </button>
-            <JavaScriptEditor />
-          </div>
+                  <PanelResizeHandle className="w-2 h-full bg-slate-300" />
+
+                  <Panel defaultSize={100} className="w-full" minSize={10}>
+                    <div className="w-full">
+                      <button className="py-2 px-6 bg-gray-900 text-blue-500 text-sm">
+                        CSS
+                      </button>
+                      <AceCodeEditor
+                        width="100%"
+                        height="400px"
+                        name="css"
+                        mode="css"
+                      />
+                    </div>
+                  </Panel>
+
+                  <PanelResizeHandle className="w-2 h-full bg-slate-300" />
+
+                  <Panel defaultSize={100} minSize={10}>
+                    <div className="w-full">
+                      <button
+                        onClick={handleToggleScript}
+                        className={`py-2 px-6 bg-gray-900 ${
+                          !toggleScript ? "text-yellow-400" : "text-blue-500"
+                        } text-sm`}
+                      >
+                        {!toggleScript ? "JS" : "TS"}
+                      </button>
+                      {!toggleScript ? (
+                        <AceCodeEditor
+                          width="100%"
+                          height="400px"
+                          name="javascript"
+                          mode="javascript"
+                        />
+                      ) : (
+                        <AceCodeEditor
+                          width="100%"
+                          height="400px"
+                          name="typescript"
+                          mode="typescript"
+                        />
+                      )}
+                    </div>
+                  </Panel>
+                </div>
+              </PanelGroup>
+            {/* </Panel> */}
+            {/* <PanelResizeHandle className="w-full h-2 bg-slate-500" /> */}
+
+            {/* <Panel defaultSize={10}> */}
+              <div className="w-full h-20 overflow-y-auto bg-slate-200 border-[4px] border-slate-700"></div>
+            {/* </Panel>
+          </PanelGroup> */}
         </div>
-
-        <div className="w-full bg-slate-200 h-28 overflow-y-auto"></div>
       </section>
     </>
   );
--- src/components/ui/aceEditor.tsx
+++ src/components/ui/aceEditor.tsx
@@ -1,28 +1,67 @@
 import AceEditor from "react-ace";
 
-import "ace-builds/src-noconflict/mode-java";
-import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-typescript";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/snippets/css";
+import "ace-builds/src-noconflict/snippets/javascript";
+import "ace-builds/src-noconflict/snippets/html";
 import "ace-builds/src-noconflict/ext-language_tools";
+import { useState } from "react";
 
 interface IAceEditorProps {
-    onChange: () => void
+  mode: string;
+  width: string;
+  height: string;
+  name: string;
+ 
+  className?: string;
 }
-export default function AceCodeEditor({ onChange }: IAceEditorProps) {
+export default function AceCodeEditor({
+  mode,
+  width,
+  height,
+  name,
+  className,
+}: IAceEditorProps) {
+
+  const [value, setValue] = useState<string>("");
+
+  function onChange(newValue: string) {
+    setValue(newValue)
+  }
+
+  console.log(value);
   return (
     <>
       <AceEditor
-        mode="java"
-        theme="github"
+        mode={mode}
+        theme="monokai"
         onChange={onChange}
-        name="UNIQUE_ID_OF_DIV"
-        editorProps={{ $blockScrolling: true }}
+        //onLoad={"onLoad"}
+        width={width}
+        height={height}
+        className={className}
+        name={name}
+        editorProps={{ $blockScrolling: Infinity }}
+        fontSize={14}
+        lineHeight={19}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={value}
         setOptions={{
           enableBasicAutocompletion: true,
           enableLiveAutocompletion: true,
           enableSnippets: true,
+          showLineNumbers: true,
+          tabSize: 2,
+          ableSnippets: true,
         }}
+        
       />
-      ,
     </>
   );
 }
