2738a983e4f24ec46e3bba1da26ffb4f2263862f
Bolaji06 <joshspencer0603@gmail.com>
2024-10-13 04:21:47 +0100

    integrate renaming and delete of workspace

---
+++ src/components/DeleteWorkspaceDialog.tsx
@@ -0,0 +1,109 @@
+/* eslint-disable */
+import { Button } from "./ui/button";
+import { Loader, Trash } from "lucide-react";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { decodeToken, getCookie } from "@/lib/utils";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { deleteWorkspace } from "@/utils/data";
+import { useNavigate } from "react-router-dom";
+import { useToast } from "@/hooks/use-toast";
+
+
+interface IDataResult {
+  success: boolean;
+  newWorkSpace: any;
+}
+interface RenameDialogProps {
+    id: string;
+    title: string;
+}
+export default function DeleteWorkSpaceDialog({ id, title }: RenameDialogProps) {
+  const token = getCookie("login");
+  const userProfile = decodeToken(token);
+
+  //@ts-ignore
+  const { parseHeader, parsePayload } = userProfile;
+
+  const [result, setResult] = useState<IDataResult | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const { toast } = useToast()
+
+  const navigate = useNavigate();
+
+  function handleOnSubmit(e: FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    const deletingWorkspace = async () => {
+      setLoading(false);
+      try {
+        setLoading(true);
+        const data = await deleteWorkspace(id);
+        setResult(data);
+
+      }catch(error){
+        console.log(error);
+      }finally{
+        setLoading(false);
+      }
+    }
+    deletingWorkspace();
+  }
+
+  useEffect(() => {
+    if (result && result?.success){
+      toast({
+        title: 'Success!',
+        description: 'Workspace deleted'
+      })
+      navigate(0);
+    }
+
+  }, [result?.success])
+
+  console.log(result)
+
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="inline-flex gap-2 justify-start bg-transparent hover:bg-transparent rounded-none py-1 text-slate-100 px-3 w-full">
+            <Trash size={18} className="text-gray-200 " />
+            <p>Delete</p>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md bg-gray-800 border-none">
+          <DialogHeader>
+            <DialogTitle className="text-slate-300">
+                Delete <span className="font-semibold">"{title}"</span>
+               
+            </DialogTitle>
+            <p className="text-slate-400">You're about to delete this workspace action can't be undone</p>
+          </DialogHeader>
+          <div className="inline-flex justify-between py-2">
+            <form onSubmit={handleOnSubmit} className="space-y-3">
+              
+              <Button disabled={loading} type="submit"
+               className="inline-flex bg-red-600 hover:bg-red-800 items-center gap-3 text-slate-100">
+                {loading ? "Deleting" : "Delete"}
+               {loading && <Loader className="animate-spin text-slate-200" size={18}/>}
+                </Button>
+            </form>
+            <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Cancel
+            </Button>
+          </DialogClose>
+
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
--- src/components/WorkspaceCard.tsx
+++ src/components/WorkspaceCard.tsx
@@ -1,7 +1,4 @@
-import { Ellipsis } from "lucide-react";
-import { Button } from "./ui/button";
 import DropDownMenu from "./ui/menu-dropdown";
-import { useEffect, useRef, useState } from "react";
 import { IWorkspace } from "@/lib/definitions";
 import { Link } from "react-router-dom";
 
@@ -9,49 +6,20 @@ interface WorkSpaceCardProps {
   props: IWorkspace;
 }
 export default function WorkSpaceCard({ props }: WorkSpaceCardProps) {
-  const [openId, setOpenId] = useState<string | null>(null);
-  const cardRef = useRef<HTMLDivElement | null>(null);
-
-  function handleToggleMenu(id: string) {
-    setOpenId((prevState) => prevState === id ? null : id);
-  }
-
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
-        setOpenId(null);
-      }
-    }
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-  
-
   return (
     <>
-      <section className="cursor-pointer" ref={cardRef}>
+      <section className="cursor-pointer">
         <div className="bg-gray-600 p-3 rounded-md w-full">
           <div className="relative bg-black/70 px-3 py-2 rounded-t-md flex justify-between items-center">
             <p className="text-gray-300 font-semibold">{props.name}</p>
 
-            <Button
-              onClick={() => handleToggleMenu(props.id)}
-              className="px-0 w-10 h-4 bg-transparent hover:bg-transparent"
-            >
-              <Ellipsis size={30} />
-            </Button>
-            {openId === props.id && (
-              <div className="absolute top-8 right-3 z-20">
-                <DropDownMenu />
-              </div>
-            )}
+            <DropDownMenu id={props.id} title={props.name} />
           </div>
           <div className="bg-slate-400 w-full relative h-36">
-            <Link to={`/playground/${props.id}`} className="py-16 w-full h-full flex justify-center items-center">
+            <Link
+              to={`/playground/${props.id}`}
+              className="py-16 w-full h-full flex justify-center items-center"
+            >
               <h1 className="text-2xl text-center font-bold text-gray-600/45">
                 Workspace
               </h1>
